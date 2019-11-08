31ba9429788721cd05e32a16633ddca6c051b442
Birger Naß <n.b@posteo.de>
2019-11-08 12:39:28 +0100

    implements react native 0.60 support

--- android/src/main/java/de/callosumSw/RNGoogleAdManager/BannerViewManager.java
+++ android/src/main/java/de/callosumSw/RNGoogleAdManager/BannerViewManager.java
@@ -1,7 +1,7 @@
 package de.callosumSw.RNGoogleAdManager;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 
